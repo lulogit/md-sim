9fb080598640e179ecd236f66213cc61139203c1
lulogit <luca1.lodi@mail.polimi.it>
2019-06-12 20:22:11 +0200

    modularization of the code

---
+++ simulation/simulation.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def car(env, name):
+    while True:
+        print('%s: Start parking at %d' % (name,env.now))
+        parking_duration = max(0,np.random.normal(5,2))
+        yield env.timeout(parking_duration)
+
+        print('%s: Start driving at %d' % (name,env.now))
+        trip_duration = max(0,np.random.normal(2,0.5))
+        yield env.timeout(trip_duration)
+
+import simpy
+env = simpy.Environment()
+env.process(car(env,"Luca"))
+env.process(car(env,"Giulio"))
+env.run(until=100)
